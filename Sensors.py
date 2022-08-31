61c4a9e69f0d57381b1d2104e88ee4e9b98ef540
Anshuman Singh <41586858+Anshuman-37@users.noreply.github.com>
2022-08-31 11:05:58 +0100

    Updated Sensors and Data_Loader

--- Sensors.py
+++ Sensors.py
@@ -9,26 +9,43 @@ class Sensor:
     def __init__(self):
         self.temp = None
         self.pressure = None
-        self.light = None
+        self.light = None ## more like control 
+        self.humidity = None 
     
 
     ## Setting the temperature Values
     def set_temperature(self):
+        '''
+        Params - Specific Temperature Values
+        Result - Set temperature to specific value
+        '''
         if(True): return 1;
         return 0;
 
     ## Setting the pressure values
     def set_pressure(self):
+        '''
+        Params - Specific Pressure Values
+        Result - Set pressure sensor to specifc value
+        '''
         if(True): return 1;
         return 0;
 
     ## Setting the light values 
     def set_light(self):
+        '''
+        Params - Light on and off 
+        Result - Set the light to specific value
+        '''
         if(True): return 1;
         return 0; 
     
     ## Modulating the humidity
     def modulate_humidity(self):
+        '''
+        Params - 
+        Result - 
+        '''
         if(self.temp and self.pressure): return 1;
         return 0;
 
